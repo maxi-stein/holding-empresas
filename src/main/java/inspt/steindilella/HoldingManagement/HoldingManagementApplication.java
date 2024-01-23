b2290c92e90b639b359fa46b393987ec304f04bd
Maximiliano Stein <maxi_stein23@hotmail.com>
2024-01-22 21:06:50 -0300

    Se inicializa el proyecto

---
+++ src/main/java/inspt/steindilella/HoldingManagement/HoldingManagementApplication.java
@@ -0,0 +1,13 @@
+package inspt.steindilella.HoldingManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoldingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoldingManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inspt/steindilella/HoldingManagement/HoldingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package inspt.steindilella.HoldingManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoldingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
