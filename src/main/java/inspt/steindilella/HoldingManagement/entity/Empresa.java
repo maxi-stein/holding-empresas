cc15e8ca4ebf3537d676482ea956d27d8759a56d
Matias Di Lella <100892403+MDL-Proyectos@users.noreply.github.com>
2024-01-29 21:30:43 -0300

    Update Empresa.java
    Agregación de atributos y métodos a la clase "EMPRESA".

--- src/main/java/inspt/steindilella/HoldingManagement/entity/Empresa.java
+++ src/main/java/inspt/steindilella/HoldingManagement/entity/Empresa.java
@@ -1,4 +1,81 @@
 package inspt.steindilella.HoldingManagement.entity;
 
-public class Empresa {
+import jakarta.persistence.*;
+@Entity
+@Table(name = "empresas")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public abstract class Empresa {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Integer id;
+
+        @Column(name = "nombre")
+        private String nombre;
+
+        @Column(name = "sede_id")
+        private String sede;
+
+        @Column(name = "fecha_inicio")
+        private Integer inicio;
+
+        @Column(name = "facturacion")
+        private String facturacion;
+
+        @Column(name = "eliminado")
+        private String eliminado;
+
+         public Empresa() {
+        }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSede() {
+        return sede;
+    }
+
+    public void setSede(String sede) {
+        this.sede = sede;
+    }
+
+    public Integer getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Integer inicio) {
+        this.inicio = inicio;
+    }
+
+    public String getFacturacion() {
+        return facturacion;
+    }
+
+    public void setFacturacion(String facturacion) {
+        this.facturacion = facturacion;
+    }
+
+    public String getEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(String eliminado) {
+        this.eliminado = eliminado;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", sede='" + sede + '\'' +
+                ", inicio=" + inicio +
+                ", facturacion='" + facturacion + '\'' +
+                ", eliminado='" + eliminado + '\'' +
+                '}';
+    }
 }
