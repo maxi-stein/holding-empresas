aed1ffff5bd090844994f9f5e39c3eca9e395bce
Maximiliano Stein <maxi_stein23@hotmail.com>
2024-01-30 23:58:59 -0300

    se agrega metodo para testear rapido persistencia de Ciudad y Pais

--- src/main/java/inspt/steindilella/HoldingManagement/HoldingManagementApplication.java
+++ src/main/java/inspt/steindilella/HoldingManagement/HoldingManagementApplication.java
@@ -1,9 +1,14 @@
 package inspt.steindilella.HoldingManagement;
 
+import inspt.steindilella.HoldingManagement.dao.UbicacionesDAO;
+import inspt.steindilella.HoldingManagement.entity.Ciudad;
+import inspt.steindilella.HoldingManagement.entity.Pais;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.math.BigInteger;
 
 @SpringBootApplication
 public class HoldingManagementApplication {
@@ -13,11 +18,18 @@ public class HoldingManagementApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner() {
-
+	public CommandLineRunner commandLineRunner(UbicacionesDAO dao) {
 		return runner -> {
-			System.out.println();
+			testSaveUbicacion(dao);
 		};
 	}
+
+	private void testSaveUbicacion(UbicacionesDAO dao) {
+		Pais pais = new Pais("Prueba",15.0,new BigInteger("100"));
+		Ciudad ciudad = new Ciudad("Prueba City",pais);
+
+		dao.save(pais,ciudad);
+	}
+
 
 }
