ed29f9e798712c6611afdf3c4f4924604898f547
Matias Di Lella <100892403+MDL-Proyectos@users.noreply.github.com>
2024-01-31 23:28:54 -0300

    Pruebas de las ultimas implementaciones
    Pruebas

--- src/main/java/inspt/steindilella/HoldingManagement/HoldingManagementApplication.java
+++ src/main/java/inspt/steindilella/HoldingManagement/HoldingManagementApplication.java
@@ -1,14 +1,11 @@
 package inspt.steindilella.HoldingManagement;
 
-import inspt.steindilella.HoldingManagement.dao.UbicacionesDAO;
-import inspt.steindilella.HoldingManagement.entity.Ciudad;
-import inspt.steindilella.HoldingManagement.entity.Pais;
+import inspt.steindilella.HoldingManagement.dao.AreasMercadoDAO;
+import inspt.steindilella.HoldingManagement.entity.AreasMercado;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.math.BigInteger;
 
 @SpringBootApplication
 public class HoldingManagementApplication {
@@ -18,17 +15,20 @@ public class HoldingManagementApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(UbicacionesDAO dao) {
+	public CommandLineRunner commandLineRunner(AreasMercadoDAO dao) {
 		return runner -> {
 			testSaveUbicacion(dao);
 		};
 	}
 
-	private void testSaveUbicacion(UbicacionesDAO dao) {
-		Pais pais = new Pais("Prueba",15.0,new BigInteger("100"));
-		Ciudad ciudad = new Ciudad("Prueba City",pais);
+	private void testSaveUbicacion(AreasMercadoDAO dao) {
+		//Pais pais = new Pais("Uruguay",1500.0,new BigInteger("100000"));
+		//Ciudad ciudad = new Ciudad("Montevideo",pais);
 
-		dao.save(pais,ciudad);
+		//dao.save(pais,ciudad);
+
+		AreasMercado areaPrueba = new AreasMercado("Consultora Financiera", "Analistas de mercados financieros.");
+		dao.save(areaPrueba);
 	}
 
 
